282fd5e2c4c2409aea5b61f90b924987acceebfc
weinanliu <44917714+weinanliu@users.noreply.github.com>
2023-09-14 16:53:51 +0800

    Create kubeadm_init.sh (#74)
    An example of creating a kube cluster by using kubeadm
    
    # Describe your changes
    
    ## Issue
    - [ ] Check this box if this PR fixes an issue, and then paste the link
    below
    - [ ] No associated issue
    
    ## Test:
    - [ ] Check this box if this PR has tests
    - [ ] No test needed

---
+++ k8s/kubeadm_init.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -x
+#
+# An example of create a kube cluster by using kubeadm
+#
+# Usage: ./kubeadm_init.sh
+# -------------------------------------------------------------------
+
+systemctl restart containerd
+
+kubeadm init \
+        --kubernetes-version=v1.24.12 \
+        --pod-network-cidr=192.168.0.0/24 \
+        --cri-socket=unix:///run/containerd/containerd.sock \
+        --image-repository registry.cn-hangzhou.aliyuncs.com/google_containers \
+        --v=5
+
+export KUBECONFIG=/etc/kubernetes/admin.conf
+
+# Make the single master node allowed to be scheduled as a worker
+kubectl taint nodes --all node-role.kubernetes.io/master-
+kubectl taint nodes --all node-role.kubernetes.io/control-plane-
+
+# If failed to apply the yaml file online, manually download it and apply the downloaded local yaml file.
+# wget https://docs.projectcalico.org/manifests/calico.yaml
+# kubectl apply -f calico.yaml
+kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
+
+# Same as above
+# wget https://raw.githubusercontent.com/NVIDIA/k8s-device-plugin/v0.14.1/nvidia-device-plugin.yml
+# kubectl create -f nvidia-device-plugin.yml
+kubectl create -f https://raw.githubusercontent.com/NVIDIA/k8s-device-plugin/v0.14.1/nvidia-device-plugin.yml
+
+# Install the GPU Operator
+kubectl apply -k node-feature-discovery/deployment/overlays/default
+helm install --wait --generate-name \
+     -n nvidia-gpu-operator --create-namespace \
+      nvidia-gpu-operator/gpu-operator \
+      --set operator.cleanupCRD=true \
+      --set psp.enabled=false \
+      --set operator.defaultRuntime=containerd \
+      --set nfd.enabled=false \
+      --set driver.enable=false \
+      --set toolkit.enabled=false
