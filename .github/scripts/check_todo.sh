278bb77cc221eaff9407c2b946b18f12a12c6cad
Yaxiong Zhao <z@nascentcore.ai>
2023-09-20 20:49:32 +0800

    Cleanup
    Signed-off-by: Yaxiong Zhao <z@nascentcore.ai>

--- .github/scripts/check_todo.sh
+++ .github/scripts/check_todo.sh
@@ -2,12 +2,12 @@
 
 echo "Checking TODO style"
 
-if grep 'todo' src helm-charts -R; then
+if grep 'todo' . -R; then
   echo "TODO should all be upper case, not todo"
   exit 1
 fi
 
-if grep 'todo:' src helm-charts -R; then
+if grep 'todo:' . -R; then
   echo "TODO should have assignee, TODO(user):, not TODO:"
   exit 1
 fi
