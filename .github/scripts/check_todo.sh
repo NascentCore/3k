5d7680cb8351ce044ad9773158a6ffe8fe2f1380
yzhao-2023 <142474494+yzhao-2023@users.noreply.github.com>
2023-09-23 00:14:15 +0800

    check_todo.sh only check golang directories

--- .github/scripts/check_todo.sh
+++ .github/scripts/check_todo.sh
@@ -2,12 +2,12 @@
 
 echo "Checking TODO style"
 
-if grep 'todo' . -R; then
+if grep 'todo' cli manager tools -R; then
   echo "TODO should all be upper case, not todo"
   exit 1
 fi
 
-if grep 'todo:' . -R; then
+if grep 'todo:' cli manager tools -R; then
   echo "TODO should have assignee, TODO(user):, not TODO:"
   exit 1
 fi
