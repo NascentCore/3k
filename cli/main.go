39bd6bc4c7b2e6d8631298846093d6c8d1a1287f
Yaxiong Zhao <z@nascentcore.ai>
2023-09-25 17:32:37 +0800

    Add test
    Signed-off-by: Yaxiong Zhao <z@nascentcore.ai>

---
+++ cli/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v2"
+)
+
+func main() {
+	app := &cli.App{
+		Name:  "login",
+		Usage: "log in onto the NascentCore.AI platform",
+		Action: func(*cli.Context) error {
+			return nil
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ common/README.md
@@ -0,0 +1,3 @@
+# Common
+
+Common utility APIs.
