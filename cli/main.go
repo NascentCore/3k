5b9820d5491267bed93da2016da1090523bcfedf
yzhao-2023 <142474494+yzhao-2023@users.noreply.github.com>
2023-09-26 11:07:32 +0800

    Basic cli (#108)
    # Describe your changes
    
    ## Issue
    - [ ] Check this box if this PR fixes an issue, and then paste the link
    below
    - [ ] No associated issue
    
    ## Test:
    - [ ] Check this box if this PR has tests
    - [ ] No test needed
    
    ---------
    
    Signed-off-by: Yaxiong Zhao <z@nascentcore.ai>

---
+++ cli/main.go
@@ -0,0 +1,28 @@
+package main
+
+// NO_TEST_NEEDED
+
+import (
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v2"
+
+	"sxwl/3k/common/errors"
+)
+
+// This is a CLI that does not do much.
+// Used as the basis for further development.
+func main() {
+	app := &cli.App{
+		Name:  "3k",
+		Usage: "log in onto the NascentCore.AI platform",
+		Action: func(*cli.Context) error {
+			return errors.UnImpl("login")
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
