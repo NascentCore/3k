5837447fc6172c4bda793d73f1dba79834aa6592
congpeiqing <peiqing@nascentcore.ai>
2023-10-09 19:17:11 +0800

    complete resource related data defination

--- manager/pkg/resource/resource_test.go
+++ manager/pkg/resource/resource_test.go
@@ -1,7 +1,20 @@
 package resource
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestGetResourceDesc(t *testing.T) {
 	// TODO:
+}
+
+func TestToJson(t *testing.T) {
+	var tmp CPodResourceDesc
+	tmp.Nodes = []Node{Node{GPUs: GPUs{Individuals: []GPU{GPU{}}}}}
+	r := tmp.ToJson()
+	fmt.Println(string(r))
+	if r == nil {
+		t.Error("marshal CPodResourceDesc failed")
+	}
 }
