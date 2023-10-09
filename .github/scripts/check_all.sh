421ac6b1c6e9c2d144cd4c065cee4145f5eafb87
yzhao-2023 <142474494+yzhao-2023@users.noreply.github.com>
2023-10-10 06:34:34 +0800

    Include all checks in check_all.sh (#125)
    # Describe your changes
    
    ## Issue
    - [ ] Check this box if this PR fixes an issue, and then paste the link
    below
    - [ ] No associated issue
    
    ## Test:
    - [ ] Check this box if this PR has tests
    - [ ] No test needed
    
    ---------
    
    Signed-off-by: Yaxiong Zhao <z@nascentcore.ai>

---
+++ .github/scripts/check_all.sh
@@ -0,0 +1,5 @@
+.github/scripts/check_markdown_filename.sh
+.github/scripts/check_todo.sh
+.github/scripts/check_dir_naming.sh
+.github/scripts/check_file_naming.sh
+.github/scripts/check_go_tests.sh
