7f729d00b63a258c0917fd216c4b0bc4cfc46bc0
Yaxiong Zhao <z@nascentcore.ai>
2023-10-10 20:31:35 +0800

    Call check_all.sh in tools/lint.sh
    Signed-off-by: Yaxiong Zhao <z@nascentcore.ai>

--- tools/lint.sh
+++ tools/lint.sh
@@ -12,37 +12,6 @@ golangci-lint run --fix --config=devops/linters/golangci.yaml
 
 echo
 print_divider
-echo "Running check_readme ..."
+echo "Running check_add ..."
 print_divider
-.github/scripts/check_readme.sh
-
-echo
-print_divider
-echo "Running check_markdown_naming ..."
-print_divider
-.github/scripts/check_markdown_filename.sh
-
-echo
-print_divider
-echo "Running check_dir_naming ..."
-print_divider
-.github/scripts/check_dir_naming.sh
-
-echo
-print_divider
-echo "Running check_file_naming ..."
-print_divider
-.github/scripts/check_file_naming.sh
-
-echo
-print_divider
-echo "Running check_todo ..."
-print_divider
-.github/scripts/check_todo.sh
-
-echo
-print_divider
-echo "Running check_go_tests ..."
-print_divider
-.github/scripts/check_go_tests.sh
-
+.github/scripts/check_all.sh
