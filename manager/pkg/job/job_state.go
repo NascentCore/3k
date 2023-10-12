a68f3a39b3ba8e68b87efa5a10ff8cc9d53fc72d
congpeiqing <peiqing@nascentcore.ai>
2023-10-12 10:26:28 +0800

    job status related data define and procedure impl

---
+++ manager/pkg/job/job_state.go
@@ -0,0 +1,17 @@
+package job
+
+// NO_TEST_NEEDED
+
+import (
+	kubeflowmpijob "sxwl/3k/manager/pkg/job/kubeflow-mpijob"
+	"sxwl/3k/manager/pkg/job/state"
+)
+
+func GetJobState() []state.State {
+	res := []state.State{}
+	//对于不同类型的任务，分别获取其任务状态，加入到结果中
+	// KubeflowMPI
+	res = append(res, kubeflowmpijob.GetState()...)
+	// more if exists
+	return res
+}
---
+++ manager/pkg/job/kubeflow-mpijob/mpijob_state.go
@@ -0,0 +1,9 @@
+package kubeflowmpijob
+
+import "sxwl/3k/manager/pkg/job/state"
+
+// NO_TEST_NEEDED
+
+func GetState() []state.State {
+	return []state.State{}
+}
