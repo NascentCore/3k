44825ff2db30504a7a38d8394fce2d94c64b8400
congpeiqing <peiqing@nascentcore.ai>
2023-10-16 18:40:03 +0800

    modeluploader sketch

--- manager/cmd/modeluploader/modeluploadjob.go
+++ manager/cmd/modeluploader/modeluploadjob.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	clientgo "sxwl/3k/manager/pkg/cluster/client-go"
+	modeluploader "sxwl/3k/manager/pkg/model-uploader"
 )
 
 // NO_TEST_NEEDED
@@ -19,16 +20,19 @@ func main() {
 	clientgo.InitClient()
 
 	//等待MPI Job完成并取得其最终的状态
-	status := UntilMPIJobFinish(mpiJobName)
+	status, err := modeluploader.UntilMPIJobFinish(mpiJobName)
+	//如果发生错误，进程异常退出，让K8 S去重启Pod，重新尝试
+	if err != nil {
+		os.Exit(1)
+	}
 	if status != "Completed" {
 		//训练出现问题，没有模型可以上传
 		return
 	}
 	//上传模型
-	err := UploadModel(bucket)
+	err = modeluploader.UploadModel(bucket)
 	//如果发生错误，进程异常退出，让K8 S去重启Pod，重新尝试
 	if err != nil {
 		os.Exit(1)
 	}
-
 }
