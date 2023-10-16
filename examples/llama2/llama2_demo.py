b284702fbb61905b3604d6240d84c038e8f6bbcd
Glen Cao <glen@Glens-MacBook-Air.local>
2023-10-15 17:08:03 -0700

    Initial changes to LLaMA 2 demo artifacts

--- examples/llama2/llama2_demo.py
+++ examples/llama2/llama2_demo.py
@@ -1,71 +1,90 @@
-import torch.nn as nn
-from transformers import LlamaForCausalLM, LlamaTokenizer, LlamaConfig
+# Standard libs.
 import argparse
+
+# 3rd-party libs.
 import torch
 from deepspeed.runtime.zero.stage3 import estimate_zero3_model_states_mem_needs_all_live
-from transformers import DataCollatorForLanguageModeling
-from transformers import Trainer, TrainingArguments
-from transformers import LineByLineTextDataset
+from transformers import (
+    LlamaConfig,
+    LlamaTokenizer,
+    LlamaForCausalLM,
+    DataCollatorForLanguageModeling,
+    LineByLineTextDataset,
+    Trainer,
+    TrainingArguments,
+)
 
 
-#parser=argparse.ArgumentParser()
+# python3 llama2_demo.py
+arg_parser = argparse.ArgumentParser()
+arg_parser.add_argument("--model_path", type=str,
+                        default="./Llama2-Chinese-7b-Chat", help="model path")
+arg_parser.add_argument("--config_path", type=str,
+                        default="./Llama2-Chinese-7b-Chat/config.json", help="model JSON config path")
+arg_parser.add_argument("--ds_config", type=str,
+                        default="./config/dp_zero3_config.json", help="DeepSpeed JSON config path")
+arg_parser.add_argument("--local-rank", type=int, default="0")
+args = arg_parser.parse_args()
 
-#parser.add_argument("--model_path",type=str, default="./Llama2-Chinese-7b-Chat/", help="model path")
-#parser.add_argument("--config_path",type=str, default="./Llama2-Chinese-7b-Chat/config.json", help="model config path")
-#parser.add_argument("--ds_config",type=str, default="./config/dp_zero3_config.json", help="deepspeed config json")
-#parser.add_argument("--local-rank", type=int, default="0")
-#args = parser.parse_args()
-
-
-config = LlamaConfig.from_pretrained("./Llama2-Chinese-7b-Chat/config.json")
-tokenizer = LlamaTokenizer.from_pretrained("./Llama2-Chinese-7b-Chat/")
+config = LlamaConfig.from_pretrained(args.config_path)
 model = LlamaForCausalLM(config)
+# Pretrained Models are set in evaluation mode by default during initialization.
+# Setting it back in training mode before doing any estimation on this model.
+# Not reqruied.
+model.train()
 #print(model)
 
-#zero3显存估算
-#estimate_zero3_model_states_mem_needs_all_live(model, num_gpus_per_node=8, num_nodes=1)
+estimate_zero3_model_states_mem_needs_all_live(model, num_gpus_per_node=8,
+                                               num_nodes=1)
 
+tokenizer = LlamaTokenizer.from_pretrained(args.model_path)
+tokenizer.pad_token = tokenizer.eos_token
 
 max_seq_length = 16
 out_model_path = "llama2_output"
-train_epoches = 10
+num_train_epochs = 10
 batch_size = 1
 
-tokenizer.pad_token = tokenizer.eos_token
+train_dataset = LineByLineTextDataset(tokenizer=tokenizer,
+                                      file_path="wikitext-103-raw/wiki.train.raw",
+                                      block_size=max_seq_length)
+eval_dataset = LineByLineTextDataset(tokenizer=tokenizer,
+                                     file_path="wikitext-103-raw/wiki.valid.raw",
+                                     block_size=max_seq_length)
+test_dataset = LineByLineTextDataset(tokenizer=tokenizer,
+                                     file_path="wikitext-103-raw/wiki.test.raw",
+                                     block_size=max_seq_length)
+torch.save(train_dataset, "data/train_dataset_ml8.pt")
+torch.save(eval_dataset, "data/eval_dataset_ml8.pt")
+torch.save(test_dataset, "data/test_dataset_ml8.pt")
 
-
-#train_file = "wikitext-103-raw/wiki.train.raw"
-#eval_file = "wikitext-103-raw/wiki.valid.raw"
-#train_dataset = LineByLineTextDataset(tokenizer=tokenizer,file_path=train_file,block_size=max_seq_length)
-#eval_dataset = LineByLineTextDataset(tokenizer=tokenizer,file_path=eval_file,block_size=max_seq_length)
-
-
-#torch.save(train_dataset,"data/train_dataset_ml8.pt")
-#torch.save(eval_dataset,"data/eval_dataset_ml8.pt")
-
-train_dataset = torch.load('data/train_dataset_ml16.pt')
-eval_dataset = torch.load('data/eval_dataset_ml16.pt')
-
-data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False) #这里bert不一样
+data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
 
 training_args = TrainingArguments(
         output_dir=out_model_path,
         overwrite_output_dir=True,
-        num_train_epochs=train_epoches,
+        do_train=True,
+        num_train_epochs=num_train_epochs,
         per_device_train_batch_size=batch_size,
+        # XXX: TBD.
+        save_strategy="steps",
+        # XXX: TBD.
         save_steps=2000,
+        # XXX: TBD.
         save_total_limit=2,
         prediction_loss_only=True,
         report_to="none",
-        deepspeed = "./config/dp_zero3_config.json"
-    )
-
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    train_dataset=train_dataset,
-    eval_dataset=eval_dataset,
-    data_collator=data_collator,
+        deepspeed=args.ds_config,
 )
 
+trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset,
+        eval_dataset=eval_dataset,
+        test_dataset=test_dataset,
+        data_collator=data_collator,
+)
+
+#trainer.train(resume_from_checkpoint=True)
 trainer.train()
