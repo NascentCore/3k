7269cc25ee977880bf81ab42fe19484a5e23ef90
Yaxiong Zhao <z@nascentcore.ai>
2023-10-17 19:00:44 +0800

    Fix
    Signed-off-by: Yaxiong Zhao <z@nascentcore.ai>

--- .github/scripts/check_dir_naming.sh
+++ .github/scripts/check_dir_naming.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
 echo "Checking directory names include only lower case chars and '-'"
-found_breakage=false
-for dirname in $(find home cli manager tools -type d); do
+find home cli manager tools -type d | while read dirname; do
   fname=$(basename ${dirname})
   if ! [[ ${fname} =~ ^[a-z-]+$ ]]; then
-    found_breakage=true
     echo "${dirname}"
+    exit 1
   fi
 done
-
-if [[ "${found_breakage}" == "true" ]]; then
-  exit 1
-fi
