5af503b76df09fd0e6784255f2f7806263821cc7
Yaxiong Zhao <z@nascentcore.ai>
2023-10-17 16:44:23 +0800

    Comment on the expected behavior of nccl_test_locally.py
    Signed-off-by: Yaxiong Zhao <z@nascentcore.ai>

--- examples/nccl/nccl_test_locally.py
+++ examples/nccl/nccl_test_locally.py
@@ -1,8 +1,8 @@
 #!/bin/env python
 #
-# An minimal usecase of NCCL by using pytorch
+# An minimal use case of NCCL by using pytorch
 #
-# Usage: ./nccl_test_locally.py
+# Usage: python nccl_test_locally.py
 # -------------------------------------------------------------------
 
 import torch
@@ -19,7 +19,9 @@ print(x2)
 
 t = time.perf_counter()
 torch.cuda.nccl.all_reduce([x1,x2])
+
 print(f'coast:{time.perf_counter() - t:.8f}s')
 
+# x1, x2 values should be identical now
 print(x1)
 print(x2)
