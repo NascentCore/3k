a130f961fb685131ad1962b58028eb71c40258b4
weinanliu <44917714+weinanliu@users.noreply.github.com>
2023-10-17 14:52:49 +0800

    Update go_worker5.sh

--- examples/nccl/go_worker5.sh
+++ examples/nccl/go_worker5.sh
@@ -6,4 +6,5 @@ WORLD_SIZE=2 \
         MASTER_ADDR=214.2.5.4 \
                 TORCH_CPP_LOG_LEVEL=INFO \
         TORCH_DISTRIBUTED_DEBUG=INFO \
+                NCCL_SOCKET_IFNAME=ens22f0 \
         python ./dist_nccl_demo.py
