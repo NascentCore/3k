be56764e0e5fda68660bee77161fbe3134dd668b
Weinan Liu <liu-weinan@qq.com>
2023-10-17 07:44:14 +0000

    nccl/nccl-tests/gpu_direct_rdma_access: submodule add

--- examples/nccl/0.build_and_push_docker_image.sh
+++ examples/nccl/0.build_and_push_docker_image.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -x
 
 # 这几句先构造base镜像上传，base镜像不会轻易改动
-docker rmi swr.cn-east-3.myhuaweicloud.com/sxwl/torch-base
-docker build . -f base.Dockerfile -t swr.cn-east-3.myhuaweicloud.com/sxwl/torch-base
-docker push swr.cn-east-3.myhuaweicloud.com/sxwl/torch-base
-exit
+#docker rmi swr.cn-east-3.myhuaweicloud.com/sxwl/torch-base
+#docker build . -f base.Dockerfile -t swr.cn-east-3.myhuaweicloud.com/sxwl/torch-base
+#docker push swr.cn-east-3.myhuaweicloud.com/sxwl/torch-base
 
+# 在base镜像基础上，加上nccl的小demo
 docker rmi -f swr.cn-east-3.myhuaweicloud.com/sxwl/for_nccl_test
 docker build -t swr.cn-east-3.myhuaweicloud.com/sxwl/for_nccl_test .
 docker push swr.cn-east-3.myhuaweicloud.com/sxwl/for_nccl_test
