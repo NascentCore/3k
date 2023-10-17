89863e92661e9375adf6e86ccec1137ea5e12bbf
Yaxiong Zhao <z@nascentcore.ai>
2023-10-17 19:01:49 +0800

    Document the limitation of variable outside of while loop
    Signed-off-by: Yaxiong Zhao <z@nascentcore.ai>

--- .github/scripts/check_dir_naming.sh
+++ .github/scripts/check_dir_naming.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 echo "Checking directory names include only lower case chars and '-'"
+# Declare variable outside of while loop, and modify its value inside
+# the loop does not work, as while loop executes in a sub-shell
 find home cli manager tools -type d | while read dirname; do
   fname=$(basename ${dirname})
   if ! [[ ${fname} =~ ^[a-z-]+$ ]]; then
