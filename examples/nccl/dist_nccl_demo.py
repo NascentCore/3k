955c5eea40a4053c88b04114f3532abd9b43f988
NascentCore.ai <eng@nascentcore.ai>
2023-10-18 08:43:20 +0800

    Specify device based on rank dist_nccl_demo.py (#148)
    This is not sufficient, as it does not consider local rank vs global
    rank
    
    # Describe your changes
    
    ## Issue
    - [ ] Check this box if this PR fixes an issue, and then paste the link
    below
    - [ ] No associated issue
    
    ## Test:
    - [ ] Check this box if this PR has tests
    - [ ] No test needed
    
    ---------
    
    Signed-off-by: Yaxiong Zhao <z@nascentcore.ai>
    Co-authored-by: Yaxiong Zhao <z@nascentcore.ai>

--- examples/nccl/dist_nccl_demo.py
+++ examples/nccl/dist_nccl_demo.py
@@ -1,25 +1,27 @@
-#!/bin/env python
 import os
+
 import torch
 import torch.distributed as dist
 
-print("RANK = " + os.environ['RANK'])
-print("WORLD_SIZE = " + os.environ['WORLD_SIZE'])
-print("MASTER_ADDR = " + os.environ['MASTER_ADDR'])
-print("MASTER_PORT = " + os.environ['MASTER_PORT'])
-os.environ['NCCL_DEBUG'] = 'INFO'
-os.environ['NCCL_DEBUG_SUBSYS'] = "INIT,P2P,NET,COLL,GRAPH"
-os.environ['NCCL_IB_DISABLE'] = '1'
-os.environ['NCCL_NET'] = 'Socket'
-os.environ['NCCL_NET_GDR_LEVEL'] = 'SYS'
+print("RANK = " + os.environ["RANK"])
+print("WORLD_SIZE = " + os.environ["WORLD_SIZE"])
+print("MASTER_ADDR = " + os.environ["MASTER_ADDR"])
+print("MASTER_PORT = " + os.environ["MASTER_PORT"])
+os.environ["NCCL_DEBUG"] = "INFO"
+os.environ["NCCL_DEBUG_SUBSYS"] = "INIT,P2P,NET,COLL,GRAPH"
+os.environ["NCCL_IB_DISABLE"] = "1"
+os.environ["NCCL_NET"] = "Socket"
+os.environ["NCCL_NET_GDR_LEVEL"] = "SYS"
 
 print(2)
 dist.init_process_group(backend="nccl")
 
 print(3)
 
-rank = int(os.environ['RANK'])
-x1 = torch.tensor([rank + 1] * 16, dtype=torch.float32).cuda()
+rank = int(os.environ["RANK"])
+cuda_dev_name = "cuda:{}".format(rank)
+print(cuda_dev_name)
+x1 = torch.tensor([rank] * 16, dtype=torch.float32, device=torch.device(cuda_dev_name))
 print(x1)
 
 dist.all_reduce(x1, op=dist.ReduceOp.SUM)
