8632b27c7c0686b1122644157e6741a5a70a7730
Glen Cao <glen@Glens-MacBook-Air.local>
2023-10-18 18:09:53 -0700

    used torch.distribute.launch to kick off the training

--- examples/llama2/llama2_demo.py
+++ examples/llama2/llama2_demo.py
@@ -1,16 +1,24 @@
 """
 Env vars:
-- RANK
-- WORLD_SIZE
-
-- MASTER_ADDR
-- MASTER_PORT
-
 - NCCL_DEBUG=INFO
-- NCCL_P2P_DISABLE=1
+- NCCL_NET=IB
 - NCCL_SOCKET_IFNAME=
 - NCCL_IB_HCA=
 - NCCL_IB_CUDA_SUPPORT=1
+- NCCL_NET_GDR_LEVEL=SYS
+- NCCL_IB_GDR_LEVEL=SYS
+- NCCL_DEBUG_SUBSYS=ALL
+
+Note:
+- "NCCL_P2P_DISABLE=1" should not be set.
+- ibdev2netdev to check NCCL_SOCKET_IFNAME and NCCL_IB_HCA
+- "NCCL_COLLNET_ENABLE=1" may need to be set.
+- "NCCL_COLLNET_NODE_THRESHOLD=17" may need to be set.
+- NCCL version
+
+torch.distributed.launch OR torchrun
+
+python3 -m torch.distributed.launch --nproc-per-node=8 --nnodes=1 --master-addr="" --master-port=
 """
 
 
@@ -34,16 +42,27 @@ from transformers import (
 
 
 if not torch.cuda.is_available():
-    sys.exit("CUDA/GPU not available on this node")
+    sys.exit("CUDA/GPU not available on this node. Exiting...")
 # This is the number of total available GPUs on this node.
 # Note: We have an env var - WORLD_SIZE, which is manully specified.
+# For testing distibuted training, we may need to launch
+# this script using MPI related commands (e.g., mpirun),
+# torch.distributed.launch, or torchrun with appropriate arguments and options.
 num_gpus = torch.cuda.device_count()
-curr_rank = int(os.environ["RANK"])
-gpu_name = f"cuda:{curr_rank}"
 
 # Linux is supposed to be always okay.
 if not torch.distributed.is_available():
-    sys.exit("`torch.distributed` package is not available")
+    sys.exit("`torch.distributed` package is not available. Exiting...")
+
+# In MPI, group -- handle -- comminucator -- processes.
+# The default (working) group is the world.
+torch.distributed.init_process_group(backend="nccl", init_method="env://")
+
+curr_rank = torch.distributed.get_rank()
+print(f"world_size: {torch.distributed.get_world_size()}")
+print(f"Current global rank: f{curr_rank}")
+
+gpu_name = f"cuda:{curr_rank}"
 
 # python3 llama2_demo.py
 arg_parser = argparse.ArgumentParser()
@@ -55,12 +74,6 @@ arg_parser.add_argument("--ds_config", type=str,
                         default="./config/dp_zero3_config.json", help="DeepSpeed JSON config path")
 arg_parser.add_argument("--local-rank", type=int, default=f"{curr_rank}")
 args = arg_parser.parse_args()
-
-# In MPI, group -- handle -- comminucator -- processes.
-# The default (working) group is the world.
-torch.distributed.init_process_group(backend="nccl",
-                                     world_size=num_gpus,
-                                     rank=curr_rank)
 
 config = LlamaConfig.from_pretrained(args.config_path)
 model = LlamaForCausalLM(config)
@@ -74,11 +87,13 @@ model.to(gpu_dev)
 model.train()
 #print(model)
 
+# A single node with multiple GPUs.
 estimate_zero3_model_states_mem_needs_all_live(model,
                                                num_gpus_per_node=num_gpus,
                                                num_nodes=1)
 
 tokenizer = LlamaTokenizer.from_pretrained(args.model_path)
+# XXX
 tokenizer.pad_token = tokenizer.eos_token
 
 max_seq_length = 16
@@ -102,19 +117,18 @@ torch.save(test_dataset, "data/test_dataset_ml8.pt")
 data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
 
 training_args = TrainingArguments(
+        log_level=debug,
         output_dir=out_model_path,
         overwrite_output_dir=True,
         do_train=True,
         num_train_epochs=num_train_epochs,
         per_device_train_batch_size=batch_size,
-        # XXX: TBD.
-        save_strategy="steps",
-        # XXX: TBD.
-        save_steps=2000,
-        # XXX: TBD.
-        save_total_limit=2,
+        save_strategy="steps",  # TBD
+        save_steps=2000,  # TBD
+        save_total_limit=2,  # TBD
         prediction_loss_only=True,
         report_to="none",
+        local_rank=curr_rank,  # XXX
         deepspeed=args.ds_config,
 )
 
