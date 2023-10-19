5c1b8819da1d5332da256f3f705da4d215d0df3c
congpeiqing <peiqing@nascentcore.ai>
2023-10-19 15:14:35 +0800

    model uploading process refinement

--- manager/cmd/modeluploader/modeluploadjob.go
+++ manager/cmd/modeluploader/modeluploadjob.go
@@ -17,22 +17,29 @@ func main() {
 	}
 	mpiJobName := os.Args[1]
 	bucket := os.Args[2]
-	clientgo.InitClient()
-
-	//等待MPI Job完成并取得其最终的状态
-	status, err := modeluploader.UntilMPIJobFinish(mpiJobName)
-	//如果发生错误，进程异常退出，让K8 S去重启Pod，重新尝试
-	if err != nil {
+	//检查上传任务是否已经开始
+	if started, err := modeluploader.CheckUploadStarted(); err != nil {
 		os.Exit(1)
+	} else if !started { //尚末开始
+		clientgo.InitClient()
+		//等待MPI Job完成并取得其最终的状态
+		status, err := modeluploader.UntilMPIJobFinish(mpiJobName)
+		//如果发生错误，进程异常退出
+		if err != nil {
+			os.Exit(1)
+		}
+		//训练出现问题，没有模型可以上传，正常结束
+		if status != modeluploader.StatusNeedUploadModel {
+			return
+		}
+		//写入开始上传标志
+		if err := modeluploader.MarkUploadStarted(); err != nil {
+			os.Exit(1)
+		}
 	}
-	if status != "Completed" {
-		//训练出现问题，没有模型可以上传
-		return
-	}
-	//上传模型
-	err = modeluploader.UploadModel(bucket)
-	//如果发生错误，进程异常退出，让K8 S去重启Pod，重新尝试
-	if err != nil {
+	//（继续）上传模型
+	//如果发生错误，进程异常退出
+	if err := modeluploader.UploadModel(bucket); err != nil {
 		os.Exit(1)
 	}
 }
--- manager/pkg/model-uploader/model_uploader.go
+++ manager/pkg/model-uploader/model_uploader.go
@@ -2,17 +2,31 @@ package modeluploader
 
 // NO_TEST_NEEDED
 
+const StatusNeedUploadModel = "Complete"
+const UploadStartedFlagFile = "/data/upload_started_flag_file"
+
 // moniter the given mpijob , until it stops (fail , finish , mannual interrupt or whatever)
 // 返回 MPIJob结束时的状态 status  和  监控过程中发生的程序必须中断不可恢复的错误 err
 // 当status为“”时，err必不为nil , status不为“”时，err必为nil。
 // 当status为“Complete”时，代表需要进一步执行模型上传工作。
 // 当err != nil 时，代表监控无法继续进行，需要由K8S触发重启。
 func UntilMPIJobFinish(mpiJobName string) (string, error) {
-	return "Complete", nil
+	return StatusNeedUploadModel, nil
 }
 
 // err != nil 代表上传任务失败，程序无法继续执行。
 // 需要由K8S触发重启
 func UploadModel(bucket string) error {
+	//上传
 	return nil
+}
+
+// 标记上传开始
+func MarkUploadStarted() error {
+	return nil
+}
+
+// 在启动时检查是否已经开始上传（应对上传中断的情况）
+func CheckUploadStarted() (bool, error) {
+	return false, nil
 }
