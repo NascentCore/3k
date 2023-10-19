36f4fdd05eb8223dac18fcd510d816ca37f026be
congpeiqing <144767397+congpeiqing@users.noreply.github.com>
2023-10-18 20:27:29 -0500

    model upload job skeleton (#154)
    # Describe your changes
    model upload job skeleton
    
    ## Issue
    - [ ] Check this box if this PR fixes an issue, and then paste the link
    below
    - [ ] No associated issue
    
    ## Test:
    - [ ] Check this box if this PR has tests
    - [ ] No test needed

---
+++ manager/pkg/model-uploader/model_uploader.go
@@ -0,0 +1,11 @@
+package modeluploader
+
+// NO_TEST_NEEDED
+
+func UntilMPIJobFinish(mpiJobName string) (string, error) {
+	return "Complete", nil
+}
+
+func UploadModel(bucket string) error {
+	return nil
+}
