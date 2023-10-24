f7e0d79f9c0c5ad2c7c3a63a3835043e75a964f5
yzhao-2023 <142474494+yzhao-2023@users.noreply.github.com>
2023-10-24 10:00:03 +0800

    Create url.go

---
+++ pkg/utils/hf/url.go
@@ -0,0 +1,10 @@
+// Package hf provides utlities for working of huggingface
+// url.go provides APIs for handling mapping from URL to fixed string
+// Used for determining ID etc.
+
+// Returns a string that is deterministic from the input HF URL,
+// the string can be used as the name of a dir
+func GetDirNameForHFUrl(url str) str {
+  // TODO(yzhao): Add impl
+  return ""
+}
