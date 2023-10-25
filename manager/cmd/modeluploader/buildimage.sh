31e295a1db138d82c9dff04a985a2161ca2c7012
congpeiqing <peiqing@nascentcore.ai>
2023-10-25 16:34:32 +0800

    add build tools for modeluploader

---
+++ manager/cmd/modeluploader/buildimage.sh
@@ -0,0 +1,2 @@
+CGO_ENABLED=0  GOOS=linux  GOARCH=amd64  go build modeluploadjob.go
+docker build -f ./Dockerfile . -t registry.cn-hangzhou.aliyuncs.com/sxwl-ai/modeluploader:`git rev-parse --short HEAD`
