0dba490993b49738dd7770ae7428907ced86c657
congpeiqing <peiqing@nascentcore.ai>
2023-11-02 15:16:14 +0800

    config for different envs

--- manager/pkg/config/const.go
+++ manager/pkg/config/const.go
@@ -2,16 +2,19 @@ package config
 
 const (
 	//cpod manager相关的配置
-	CPOD_NAMESPACE               = "cpod"                     //CPod 所工作的NameSpace
-	PORTAL_JOBTYPE_MPI           = "MPI"                      //Portal中MPI JobType的表示
-	URLPATH_FETCH_JOB            = "/api/userJob/cpod_jobs"   //获取Job的Path
-	URLPATH_UPLOAD_CPOD_STATUS   = "/api/userJob/cpod_status" //上传信息的Path
-	CPOD_CREATED_PVC_ACCESS_MODE = "ReadWriteMany"            //创建PVC时所指定的Access Mode
+	CPOD_NAMESPACE                = "cpod"                     //CPod 所工作的NameSpace
+	PORTAL_JOBTYPE_MPI            = "MPI"                      //Portal中MPI JobType的表示
+	URLPATH_FETCH_JOB             = "/api/userJob/cpod_jobs"   //获取Job的Path
+	URLPATH_UPLOAD_CPOD_STATUS    = "/api/userJob/cpod_status" //上传信息的Path
+	CPOD_CREATED_PVC_ACCESS_MODE  = "ReadWriteMany"            //创建PVC时所指定的Access Mode
+	OSS_ACCESS_KEY_ENV_NAME       = "AK"                       //oss access key在ModelUploader所在容器的环境中的名称，同时代表其在K8S Secret中所对应的Key的名称
+	OSS_ACCESS_SECRET_ENV_NAME    = "AS"                       //oss access secret在ModelUploader所在容器的环境中的名称，同时代表其在K8S Secret中所对应的Key的名称
+	K8S_SECRET_NAME_FOR_OSS       = "akas4oss"                 //记录K8S访问密码的K8S secret的名称（运行CpodManager之前必须已经创建好）
+	K8S_SA_NAME_FOR_MODELUPLOADER = "sa-modeluploader"         //Modeluploader用来访问集群所使用的 Service Account Name（运行CpodManager之前必须已经创建好）
 	//Model Uploader 相关的配置
 	MODELUPLOADER_PVC_MOUNT_PATH = "/data"                              //在Pod中的PVC挂载路径
 	STATUS_NEEDS_UPLOAD_MODEL    = "Complete"                           //需要上传模型时MPIJob的状态
 	UPLOAD_STARTED_FLAG_FILE     = "upload_started_flag_file"           //标识上传工作开始的文件的名称
 	FILE_UPLOAD_LOG_FILE         = "files_uploaded_log"                 //记录上传工作进度的文件的名称
 	OSS_ENDPOINT                 = "http://oss-cn-beijing.aliyuncs.com" //阿里云OSS Endpoint
-	OSS_BUCKET                   = "sxwl-ai"                            //OSS Bucket
 )
--- manager/pkg/config/fromenv.go
+++ manager/pkg/config/fromenv.go
@@ -3,9 +3,17 @@ package config
 import "os"
 
 var (
+	//for cpod manager
 	STORAGE_CLASS_TO_CREATE_PVC = os.Getenv("StorageClass") //创建PVC所指定的SC，在CPod集群中需要提前创建好
 	MODELUPLOADER_IMAGE         = os.Getenv("UPLOADIMAGE")  //ModelUploader的Docker Image ， 需要在Cpod集群上能够Pull
-	OSS_ACCESS_KEY              = os.Getenv("AK")
-	OSS_ACCESS_SECRET           = os.Getenv("AS")
 	DEPLOY                      = os.Getenv("DEPLOY")
+	//for model uploader
+	OSS_ACCESS_KEY    = os.Getenv(OSS_ACCESS_KEY_ENV_NAME)
+	OSS_ACCESS_SECRET = os.Getenv(OSS_ACCESS_SECRET_ENV_NAME)
+	OSS_BUCKET        = func() string { //OSS Bucket
+		if DEPLOY == "PROD" {
+			return "sxwl-ai"
+		}
+		return "sxwl-ai-test"
+	}()
 )
