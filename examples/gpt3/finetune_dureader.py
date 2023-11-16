e86dc90490653b7c3416552570c9b055a8fb78c5
yzhao-2023 <142474494+yzhao-2023@users.noreply.github.com>
2023-11-16 21:58:58 +0800

    Create finetune_dureader.py

---
+++ examples/gpt3/finetune_dureader.py
@@ -0,0 +1,59 @@
+# The code below is copied from the "输入输出形式训练" section of
+# https://modelscope.cn/models/damo/nlp_gpt3_text-generation_1.3B/summary
+
+# finetune_dureader.py
+from torch.utils.tensorboard import SummaryWriter
+from modelscope.msdatasets import MsDataset
+from modelscope.trainers import build_trainer
+from modelscope.metainfo import Trainers
+
+
+dataset_dict = MsDataset.load('DuReader_robust-QG')
+
+train_dataset = dataset_dict['train'].remap_columns({'text1': 'src_txt', 'text2': 'tgt_txt'}) \
+    .map(lambda example: {'src_txt': example['src_txt'] + '\n'})
+eval_dataset = dataset_dict['validation'].remap_columns({'text1': 'src_txt', 'text2': 'tgt_txt'}) \
+    .map(lambda example: {'src_txt': example['src_txt'] + '\n'})
+
+max_epochs = 10
+
+tmp_dir = './gpt3_dureader'
+
+num_warmup_steps = 200
+
+def noam_lambda(current_step: int):
+    current_step += 1
+    return min(current_step**(-0.5),
+               current_step * num_warmup_steps**(-1.5))
+
+def cfg_modify_fn(cfg):
+    cfg.train.lr_scheduler = {
+        'type': 'LambdaLR',
+        'lr_lambda': noam_lambda,
+        'options': {
+            'by_epoch': False
+        }
+    }
+    cfg.train.optimizer = {'type': 'AdamW', 'lr': 1e-4}
+    cfg.train.dataloader = {
+        'batch_size_per_gpu': 4,
+        'workers_per_gpu': 1
+    }
+    cfg.train.hooks.append({
+        'type': 'MegatronHook'
+    })
+    cfg.preprocessor.sequence_length = 512
+    cfg.model.checkpoint_model_parallel_size = 1
+    return cfg
+
+kwargs = dict(
+    model='damo/nlp_gpt3_text-generation_1.3B',
+    train_dataset=train_dataset,
+    eval_dataset=eval_dataset,
+    max_epochs=max_epochs,
+    work_dir=tmp_dir,
+    cfg_modify_fn=cfg_modify_fn)
+
+trainer = build_trainer(
+    name=Trainers.gpt3_trainer, default_args=kwargs)
+trainer.train()
