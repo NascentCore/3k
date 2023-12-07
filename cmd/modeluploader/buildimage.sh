03f5b566ec25aefab73938e019c59173565d409a
congpeiqing <peiqing@nascentcore.ai>
2023-12-07 14:27:02 +0800

    fix lint error

--- cmd/modeluploader/buildimage.sh
+++ cmd/modeluploader/buildimage.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 CGO_ENABLED=0  GOOS=linux  GOARCH=amd64  go build modeluploadjob.go
-docker build -f ./Dockerfile . -t registry.cn-hangzhou.aliyuncs.com/sxwl-ai/modeluploader:`git rev-parse --short HEAD`
+docker build -f ./Dockerfile . -t registry.cn-hangzhou.aliyuncs.com/sxwl-ai/modeluploader:$(git rev-parse --short HEAD)
