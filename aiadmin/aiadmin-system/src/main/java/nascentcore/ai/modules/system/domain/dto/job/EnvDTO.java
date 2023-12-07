4462bbba98b65941c125a8e79ba84e73778d8256
jimgreat666 <142968359+jimgreat666@users.noreply.github.com>
2023-12-07 16:57:23 +0800

    修改MODIHAND_OPEN_NODE_TOKEN会自动转成小写问题 (#358)
    ### 背景
    > 描述这个 Pull Request 之前代码的状态
    
    ### 目标
    > 描述这个 Pull Request 要完成的功能
    
    ### 其他信息
    > 描述这个 Pull Request 相关的其他信息

--- aiadmin/aiadmin-system/src/main/java/nascentcore/ai/modules/system/domain/dto/job/EnvDTO.java
+++ aiadmin/aiadmin-system/src/main/java/nascentcore/ai/modules/system/domain/dto/job/EnvDTO.java
@@ -1,12 +1,12 @@
 package nascentcore.ai.modules.system.domain.dto.job;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.alibaba.fastjson.annotation.JSONField;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @Data
 public class EnvDTO {
-    @JsonProperty("MODIHAND_OPEN_NODE_TOKEN")
+    @JSONField(name ="MODIHAND_OPEN_NODE_TOKEN")
     private String modihandOpenNodeToken;
 }
