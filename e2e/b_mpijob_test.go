70f3961a44c2de53c566985215c4776f51173eb0
DongGang <150807211+sxwl-donggang@users.noreply.github.com>
2023-12-08 16:58:09 +0800

    feat: add e2e (#351)
    ### 背景
    没有e2e测试，有一些case记录在examples
    
    ### 目标
    引入e2e，实现e2e测试自动化
    
    ### 其他信息
    > 描述这个 Pull Request 相关的其他信息
    
    Co-authored-by: congpeiqing <144767397+congpeiqing@users.noreply.github.com>

---
+++ e2e/b_mpijob_test.go
@@ -0,0 +1,78 @@
+package e2e
+
+import (
+	"context"
+	"os"
+	"testing"
+	"time"
+
+	mpiv2beta "github.com/kubeflow/mpi-operator/pkg/apis/kubeflow/v2beta1"
+	tov1 "github.com/kubeflow/training-operator/pkg/apis/kubeflow.org/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/e2e-framework/klient/decoder"
+	"sigs.k8s.io/e2e-framework/klient/k8s"
+	"sigs.k8s.io/e2e-framework/klient/k8s/resources"
+	"sigs.k8s.io/e2e-framework/klient/wait"
+	"sigs.k8s.io/e2e-framework/klient/wait/conditions"
+	"sigs.k8s.io/e2e-framework/pkg/envconf"
+	"sigs.k8s.io/e2e-framework/pkg/features"
+)
+
+func TestMPIJob(t *testing.T) {
+	featBuilder := features.New("MPIJobs")
+
+	featBuilder.Assess("MPIJob mnist example", func(ctx context.Context, t *testing.T, c *envconf.Config) context.Context {
+		r, err := resources.New(cfg.Client().RESTConfig())
+		if err != nil {
+			t.Fail()
+		}
+		mpiv2beta.AddToScheme(r.GetScheme())
+		r.WithNamespace(namespace)
+
+		err = decoder.DecodeEachFile(ctx, os.DirFS("./mpijob"), "*", decoder.CreateHandler(r), decoder.MutateNamespace(namespace))
+		if err != nil {
+			t.Fatalf("Failed due to error: %s", err)
+		}
+
+		return ctx
+	}).Assess("MPIJob Check", func(ctx context.Context, t *testing.T, c *envconf.Config) context.Context {
+		mpijob := mpiv2beta.MPIJob{
+			ObjectMeta: metav1.ObjectMeta{Name: "tensorflow-mnist", Namespace: namespace},
+		}
+		t.Logf("MPIJob Checking")
+		err := wait.For(conditions.New(cfg.Client().Resources()).ResourceMatch(&mpijob, func(object k8s.Object) bool {
+			t.Logf("MPIJob Checking ...")
+			mj := object.(*mpiv2beta.MPIJob)
+			for _, cond := range mj.Status.Conditions {
+				if cond.Type == mpiv2beta.JobSucceeded && cond.Status == corev1.ConditionTrue {
+					t.Logf("MPIJob Check Successed")
+					return true
+				}
+			}
+			return false
+		}), wait.WithImmediate(), wait.WithInterval(10*time.Second), wait.WithTimeout(2*time.Minute))
+		if err != nil {
+			t.Errorf("MPIJob Check Failed")
+		}
+		return ctx
+	})
+
+	featBuilder.Teardown(func(ctx context.Context, t *testing.T, c *envconf.Config) context.Context {
+		r, err := resources.New(cfg.Client().RESTConfig())
+		if err != nil {
+			t.Fail()
+		}
+		tov1.AddToScheme(r.GetScheme())
+		r.WithNamespace(namespace)
+
+		err = decoder.DecodeEachFile(ctx, os.DirFS("./mpijob"), "*", decoder.DeleteHandler(r), decoder.MutateNamespace(namespace))
+
+		if err != nil {
+			t.Fatalf("Failed teardown due to error: %s", err)
+		}
+		return ctx
+	})
+
+	testenv.Test(t, featBuilder.Feature())
+}
--- go.mod
+++ go.mod
@@ -6,6 +6,8 @@ require github.com/urfave/cli/v2 v2.25.7
 
 require (
 	github.com/aliyun/aliyun-oss-go-sdk v2.2.9+incompatible
+	github.com/kubeflow/mpi-operator v0.4.0
+	github.com/kubeflow/training-operator v1.7.0
 	github.com/spf13/cobra v1.8.0
 	go.uber.org/zap v1.26.0
 	k8s.io/api v0.28.2
@@ -13,10 +15,13 @@ require (
 	k8s.io/client-go v0.28.2
 )
 
+require github.com/kubeflow/common v0.4.6 // indirect
+
 require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.3 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/emicklei/go-restful/v3 v3.9.0 // indirect
+	github.com/emicklei/go-restful/v3 v3.10.2 // indirect
+	github.com/evanphx/json-patch/v5 v5.6.0 // indirect
 	github.com/go-logr/logr v1.2.4 // indirect
 	github.com/go-openapi/jsonpointer v0.19.6 // indirect
 	github.com/go-openapi/jsonreference v0.20.2 // indirect
@@ -27,17 +32,21 @@ require (
 	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/google/gofuzz v1.2.0 // indirect
 	github.com/google/uuid v1.3.0 // indirect
-	github.com/imdario/mergo v0.3.6 // indirect
+	github.com/imdario/mergo v0.3.15 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/moby/spdystream v0.2.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/sirupsen/logrus v1.9.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/stretchr/testify v1.8.3 // indirect
+	github.com/vladimirvivien/gexe v0.2.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/net v0.13.0 // indirect
@@ -54,6 +63,8 @@ require (
 	k8s.io/klog/v2 v2.100.1 // indirect
 	k8s.io/kube-openapi v0.0.0-20230717233707-2695361300d9 // indirect
 	k8s.io/utils v0.0.0-20230406110748-d93618cff8a2 // indirect
+	sigs.k8s.io/controller-runtime v0.15.1 // indirect
+	sigs.k8s.io/e2e-framework v0.3.0
 	sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd // indirect
 	sigs.k8s.io/structured-merge-diff/v4 v4.2.3 // indirect
 	sigs.k8s.io/yaml v1.3.0 // indirect
