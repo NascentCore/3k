c27dceed8d8fffdbf17c267dddea34212418f750
jim <jiming@nascentcore.ai>
2023-12-09 15:50:56 +0800

    fix api test for create userjob

--- e2e/api/api_test.go
+++ e2e/api/api_test.go
@@ -2,9 +2,11 @@ package api
 
 import (
     "bytes"
-    "testing"
-    "net/http"
     "encoding/json"
+    "fmt"
+    "io"
+    "net/http"
+    "testing"
 )
 
 const URL = "https://llm.nascentcore.cn/api/userJob"
@@ -38,4 +40,26 @@ func TestPostUserJob(t *testing.T) {
         t.Fail()
     }
     req.Header.Set("Content-Type", "application/json")
+    //不能checkin 内部测试token不能暴露到互联网上，容易泄露内部测试环境
+    req.Header.Set("Authorization", "Bearer eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJiNjdhMjAwNzljZDY0NmE4YThlNWM3ZDY1ZDJhOTcxNSIsInVzZXIiOiJqaW0xIiwic3ViIjoiamltMSJ9.K_8kcdXOUhoDIp5pZX48MC1g6Q_Ut0zXVO5V2heh3AgJpO9BJqjix61lM3Z_gEK4L133MFkQbtE9EiY0HyfM1A")
+    client := &http.Client{}
+    resp, err := client.Do(req)
+    if err != nil {
+        t.Fail()
+    }
+    defer resp.Body.Close()
+    var result map[string]interface{}
+    body,err := io.ReadAll(resp.Body)
+    if err != nil {
+        t.Fail()
+    }
+    print("body",body)
+    err = json.Unmarshal(body, &result)
+    if err != nil {
+        t.Fail()
+    }
+    for k,v := range result{
+        fmt.Println(k,v)
+    }
+    //println("result",result)
 }
