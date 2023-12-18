183189058c11ffd6e8f9a41fc6b1f8e560992107
yzhao-2023 <142474494+yzhao-2023@users.noreply.github.com>
2023-12-18 08:45:23 +0800

    Add todo to use command line flags in main.go (#400)

--- cmd/cloud/main.go
+++ cmd/cloud/main.go
@@ -19,32 +19,34 @@ func main() {
 		fmt.Println("Usage : sxwlapitest  [ need createjob url ] [gpunumber] [gputype] [token]")
 		os.Exit(1)
 	}
+
+	// TODO: 使用命令行参数 https://gobyexample.com/command-line-flags
 	url := os.Args[1]
 	token := os.Args[4]
 	gpuNum := os.Args[2]
 	gpuType := os.Args[3]
 	jobStr := `
-      {
-        "jobName": "ai1b4f3a3f-17f2-4257-a1e8-f49e1641176a",
-        "pretrainedModelName": "chatglm3-6b",
-        "imagePath": "dockerhub.kubekey.local/kubesphereio/sxwl-ai/bert:withcmd",
-        "ckptPath": "/data",
-        "datasetName": "",
-        "modelPath": "/model",
-        "stopType": 0,
-        "env": {
-            "TESTENV": "1111"
-        },
-        "modelVol": 100,
-        "datasetPath": "",
-        "gpuNumber": 1,
-        "pretrainedModelPath": "/pretrainedModel",
-        "ckptVol": 100,
-        "runCommand": "sleep 300",
-        "callbackUrl": "https://cloud.nascentcore.cn/api/test/callback",
-        "jobType": "GeneralJob",
-        "gpuType": "NVIDIA-GeForce-RTX-3090"
-      }`
+        {
+	        "jobName": "ai1b4f3a3f-17f2-4257-a1e8-f49e1641176a",
+	        "pretrainedModelName": "chatglm3-6b",
+	        "imagePath": "dockerhub.kubekey.local/kubesphereio/sxwl-ai/bert:withcmd",
+	        "ckptPath": "/data",
+	        "datasetName": "",
+	        "modelPath": "/model",
+	        "stopType": 0,
+	        "env": {
+	            "TESTENV": "1111"
+	        },
+	        "modelVol": 100,
+	        "datasetPath": "",
+	        "gpuNumber": 1,
+	        "pretrainedModelPath": "/pretrainedModel",
+	        "ckptVol": 100,
+	        "runCommand": "sleep 300",
+	        "callbackUrl": "https://cloud.nascentcore.cn/api/test/callback",
+	        "jobType": "GeneralJob",
+	        "gpuType": "NVIDIA-GeForce-RTX-3090"
+        }`
 	job := communication.RawJobDataItem{}
 	json.Unmarshal([]byte(jobStr), &job)
 	gpuCnt, _ := strconv.Atoi(gpuNum)
