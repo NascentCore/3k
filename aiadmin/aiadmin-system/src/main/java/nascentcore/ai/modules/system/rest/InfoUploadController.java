dba7817a70b14765dddfd83172daa250e0393a45
jim <jiming@nascentcore.ai>
2023-12-19 17:41:05 +0800

    support go-scheduler for upload_status and cpod_status

--- aiadmin/aiadmin-system/src/main/java/nascentcore/ai/modules/system/rest/InfoUploadController.java
+++ aiadmin/aiadmin-system/src/main/java/nascentcore/ai/modules/system/rest/InfoUploadController.java
@@ -1,32 +1,21 @@
 package nascentcore.ai.modules.system.rest;
 
-import cn.hutool.core.collection.CollectionUtil;
-import cn.hutool.core.date.DateTime;
-import com.alibaba.fastjson.JSON;
+import cn.hutool.http.HttpRequest;
+import cn.hutool.http.HttpResponse;
+import cn.hutool.http.HttpUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import nascentcore.ai.config.ServiceProperties;
+import nascentcore.ai.exception.BadRequestException;
 import nascentcore.ai.modules.system.domain.Fileurl;
-import nascentcore.ai.modules.system.domain.UserJob;
-import nascentcore.ai.modules.system.domain.bean.Constants;
-import nascentcore.ai.modules.system.domain.dto.file.ModelUrl;
 import nascentcore.ai.modules.system.domain.vo.FileurlQueryCriteria;
-import nascentcore.ai.modules.system.domain.vo.UserJobQueryCriteria;
 import nascentcore.ai.modules.system.service.FileurlService;
-import nascentcore.ai.modules.system.service.UserJobService;
-import nascentcore.ai.modules.system.thread.HttpSenddateThread;
-import nascentcore.ai.utils.DateUtil;
-import nascentcore.ai.utils.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.TimeZone;
-
-import static java.util.TimeZone.getTimeZone;
 
 @RestController
 @RequiredArgsConstructor
@@ -35,22 +24,20 @@ import static java.util.TimeZone.getTimeZone;
 public class InfoUploadController {
 
     private final FileurlService fileurlService;
-    private final UserJobService userJobService;
+    private final ServiceProperties serviceProperties;
     @PostMapping(value = "/upload_status")
     @ApiOperation("上传模型数据文件 url")
     public ResponseEntity<Object> uploadStatus(@Validated @RequestBody String resources) {
-        ModelUrl modelUrl = JSON.parseObject(resources, ModelUrl.class);
-        List<Fileurl> fileurlList = new ArrayList<>();
-        if (null != modelUrl && CollectionUtil.isNotEmpty(modelUrl.getDownloadUrls())) {
-            for (String it : modelUrl.getDownloadUrls()) {
-                Fileurl fileurl = new Fileurl();
-                fileurl.setJobName(modelUrl.getJobName());
-                fileurl.setFileUrl(it);
-                fileurl.setCreateTime(DateUtil.getUTCTimeStamp());
-                fileurl.setUpdateTime(DateUtil.getUTCTimeStamp());
-                fileurlList.add(fileurl);
+        try {
+            String url = serviceProperties.getServices().getScheduleService() + "info/upload_status";
+            HttpRequest request = HttpUtil.createPost(url);
+            request.body(resources);
+            HttpResponse execute = request.execute();
+            if (!execute.isOk()) {
+                throw new BadRequestException("服务异常，调用调度模块upload_status接口异常");
             }
-            fileurlService.save(fileurlList);
+        } catch (Exception e) {
+            throw new BadRequestException("服务异常，调用调度模块upload_status接口异常");
         }
         return new ResponseEntity<>(HttpStatus.OK);
     }
