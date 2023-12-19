874bc9d4a752390e2cfe3b971f2400be60541747
jim <jiming@nascentcore.ai>
2023-12-19 18:41:28 +0800

    add go-scheduler for upload_status

--- gomicro/scheduler/internal/handler/routes.go
+++ gomicro/scheduler/internal/handler/routes.go
@@ -17,6 +17,11 @@ func RegisterHandlers(server *rest.Server, serverCtx *svc.ServiceContext) {
 				Path:    "/cpod/status",
 				Handler: CpodStatusHandler(serverCtx),
 			},
+			{
+				Method:  http.MethodPost,
+				Path:    "/info/upload_status",
+				Handler: UploadStatusHandler(serverCtx),
+			},
 		},
 	)
 }
