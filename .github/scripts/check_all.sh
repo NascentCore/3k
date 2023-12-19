593769df1d36027c7c9f8208d9580bb1e5cb94c7
Yaxiong Zhao <z@sxwl.ai>
2023-12-19 19:59:34 +0800

    Remove check_todo.sh which is not so useful
    Signed-off-by: Yaxiong Zhao <z@sxwl.ai>

--- .github/scripts/check_all.sh
+++ .github/scripts/check_all.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -e
 
 .github/scripts/check_markdown_filename.sh
-.github/scripts/check_todo.sh
 .github/scripts/check_dir_naming.sh
 .github/scripts/check_file_naming.sh
 .github/scripts/check_go_tests.sh
