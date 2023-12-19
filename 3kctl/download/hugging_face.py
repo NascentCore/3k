ec35702470a614dbb8c39acf4b13791f35ed9e61
chenshu <152252984+guochenshu@users.noreply.github.com>
2023-12-19 20:13:59 +0800

    下载器支持dataset (#411)

--- 3kctl/download/hugging_face.py
+++ 3kctl/download/hugging_face.py
@@ -5,9 +5,12 @@ class HuggingFaceHub(object):
     """Simple encapsulation of huggingface"""
 
     def have_model(self, model_id):
-        return repo_exists(model_id)
+        return repo_exists(repo_id=model_id)
 
-    def size(self, model_id):
+    def have_dataset(self, dataset_id):
+        return repo_exists(repo_id=dataset_id, repo_type="dataset")
+
+    def model_size(self, model_id):
         """return the size of model in GB"""
 
         total_size = 0
@@ -17,7 +20,22 @@ class HuggingFaceHub(object):
 
         return total_size / 1024 / 1024 / 1024
 
-    def git_url(self, model_id):
+    def dataset_size(self, dataset_id):
+        """return the size of dataset in GB"""
+
+        total_size = 0
+        file_list = list_files_info(dataset_id, repo_type="dataset")
+        for file in file_list:
+            total_size += file.size
+
+        return total_size / 1024 / 1024 / 1024
+
+    def git_model_url(self, model_id):
         """return the git url by model id"""
 
         return "https://huggingface.co/%s" % model_id
+
+    def git_dataset_url(self, dataset_id):
+        """return the git url by model id"""
+
+        return "https://huggingface.co/datasets/%s" % dataset_id
