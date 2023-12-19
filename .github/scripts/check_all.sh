3f75e4720c5a3957068446aaa4504bbb34bae6dc
Yaxiong Zhao <z@sxwl.ai>
2023-12-19 20:42:39 +0800

    Rename check_file_naming.sh
    Signed-off-by: Yaxiong Zhao <z@sxwl.ai>

--- .github/scripts/check_all.sh
+++ .github/scripts/check_all.sh
@@ -2,6 +2,5 @@
 
 .github/scripts/check_markdown_filename.sh
 .github/scripts/check_dir_naming.sh
-.github/scripts/check_file_naming.sh
 .github/scripts/check_go_tests.sh
 .github/scripts/check_dockerfile.sh
