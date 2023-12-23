1edc0da4d11a12731c90b95449938246f2bcd811
jim <jiming@nascentcore.ai>
2023-12-23 11:00:58 +0800

    add TestUploadStatusLogic_UploadStatus

--- gomicro/scheduler/internal/logic/upload_status_logic_test.go
+++ gomicro/scheduler/internal/logic/upload_status_logic_test.go
@@ -2,8 +2,21 @@ package logic
 
 import (
 	"testing"
+
+	"sxwl/3k/gomicro/scheduler/internal/svc"
+	"sxwl/3k/gomicro/scheduler/internal/types"
 )
 
 func TestUploadStatusLogic_UploadStatus(t *testing.T) {
-
+	ctx := svc.ServiceContext{}
+	uploads := UploadStatusLogic{
+		svcCtx: &ctx,
+	}
+	modelurlReq := types.ModelUrlReq{
+		DownloadUrls: []string{},
+	}
+	_, err := uploads.UploadStatus(&modelurlReq)
+	if err != nil {
+		t.Fail()
+	}
 }
