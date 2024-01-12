7d7d1313b2a6f6c0ca3f38677c72511cd3e84483
Yaxiong Zhao <z@sxwl.ai>
2024-01-12 12:58:17 -0800

    Add parse()
    Signed-off-by: Yaxiong Zhao <z@sxwl.ai>

--- pkg/utils/config/file.go
+++ pkg/utils/config/file.go
@@ -1,6 +1,23 @@
 package config
 
+import (
+	"os"
+	"strings"
+)
+
 // Reads the content of 'f' and return a key value map of the config entries.
 func Read(f string) map[string]string {
-  return map[string]string{}
+	text := os.ReadFile(f)
+	return parse(text)
+}
+
+func parse(text string) map[string]string {
+	res := make(map[string]string)
+	lines := strings.Split(text, "\n")
+	for _, line := range lines {
+		line = strings.TrimSpace(line)
+		kv := strings.Split(line, ":")
+		res[kv[0]] = strings.TrimSpace(kv[1])
+	}
+	return res
 }
--- pkg/utils/config/file_test.go
+++ pkg/utils/config/file_test.go
@@ -1,1 +1,16 @@
 package config
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestParse(t *testing.T) {
+	got := parse("a: test")
+	expected := map[string]string {
+		"a": "test",
+	}
+	if !reflect.DeepEqual(got, expected) {
+		t.Errorf("got %v expect %v", got, expected)
+	}
+}
