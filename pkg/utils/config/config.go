33761171637bc26e32193a98d5a4e49f43011795
Yaxiong Zhao <142474494+yzhao-2023@users.noreply.github.com>
2024-01-12 21:41:27 -0800

    Add config apis for reading file and env vars (#473)
    Used for future code to have unified APIs to read configs.
    
    ---------
    
    Signed-off-by: Yaxiong Zhao <z@sxwl.ai>
    Co-authored-by: Yaxiong Zhao <z@sxwl.ai>

--- pkg/utils/config/config.go
+++
@@ -1,6 +0,0 @@
-package config
-
-// Reads the content of 'f' and return a key value map of the config entries.
-func Read(_ string) map[string]string {
-  return map[string]string{}
-}
---
+++ pkg/utils/config/file.go
@@ -0,0 +1,27 @@
+package config
+
+import (
+	"os"
+	"strings"
+)
+
+// Reads the content of 'f' and return a key value map of the config entries.
+// The file content is a ':' separated key value entries, see examples in the test file.
+func Read(f string) (map[string]string, error) {
+	text, err := os.ReadFile(f)
+	if err != nil {
+		return nil, err
+	}
+	return parse(string(text)), nil
+}
+
+func parse(text string) map[string]string {
+	res := make(map[string]string)
+	lines := strings.Split(text, "\n")
+	for _, line := range lines {
+		line = strings.TrimSpace(line)
+		kv := strings.Split(line, ":")
+		res[kv[0]] = strings.TrimSpace(kv[1])
+	}
+	return res
+}
