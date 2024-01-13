292c8d5c0b17cd9d3c3619d91b14afcb5676750f
Yaxiong Zhao <142474494+yzhao-2023@users.noreply.github.com>
2024-01-13 00:31:00 -0800

    Add basic testing APIs (#481)
    Signed-off-by: Yaxiong Zhao <z@sxwl.ai>
    Co-authored-by: Yaxiong Zhao <z@sxwl.ai>

--- pkg/testing/fs_test.go
+++ pkg/testing/fs_test.go
@@ -1,1 +1,19 @@
 package testing
+
+import (
+	"testing"
+
+	"sxwl/3k/pkg/utils/fs"
+)
+
+func TestCreateTmpFile(t *testing.T) {
+	p := CreateTmpFile()
+	if !fs.Exists(p) {
+		t.Errorf("%s should be created, but does not exist", p)
+	}
+
+	p = CreateTmpFileWithContent("content")
+	if MustReadFile(p) != "content" {
+		t.Errorf("p is not empty, %s", p)
+	}
+}
