e4d793b1b998e60530aa258e776cc168d90e24ea
congpeiqing <peiqing@nascentcore.ai>
2024-01-16 16:00:55 +0800

    delete cpodmanager portalsynch

--- cmd/cpodmanager/buildimage.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-CGO_ENABLED=0  GOOS=linux  GOARCH=amd64  go build cpodmanager.go
-docker build -f ./Dockerfile . -t sxwl-registry.cn-beijing.cr.aliyuncs.com/sxwl-ai/cpodmanager:$(git rev-parse --short HEAD)
-docker push sxwl-registry.cn-beijing.cr.aliyuncs.com/sxwl-ai/cpodmanager:$(git rev-parse --short HEAD)
