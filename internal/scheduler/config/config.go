15f6ebf25d35f3138cf950ff8a75fb72be3aafb4
guochenshu <cs@sxwl.ai>
2024-01-26 10:02:55 +0800

    modify fine-tune logic

--- internal/scheduler/config/config.go
+++ internal/scheduler/config/config.go
@@ -10,7 +10,8 @@ type Config struct {
 		DataSource string
 	} `json:"DB,optional" `
 	FinetuneModel map[string]struct {
-		Image  string `json:"image"`
-		GPUMem int64  `json:"gpu_mem"`
+		Image   string `json:"image"`
+		GPUMem  int64  `json:"gpu_mem"`
+		Command string `json:"command"`
 	} `json:"FinetuneModel"`
 }
