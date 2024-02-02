ef612c1d517651c0c11bd00db4597f09858379ac
cairong <146914987+cairong-ai@users.noreply.github.com>
2024-02-02 18:32:53 +0800

    Update make-package.sh

--- deployment/manifest/make-package.sh
+++ deployment/manifest/make-package.sh
@@ -5,7 +5,7 @@ ARTIFACT_DIR=${OFFLINE_DIR}/artifact
 cp ../values/* ${ARTIFACT_DIR}/deploy/values
 cp ../yaml_apps/* ${ARTIFACT_DIR}/deploy/yaml_apps
 cp -a ../../3kctl/* ${ARTIFACT_DIR}/cli/
-cp manifact.yaml ${OFFLINE_DIR}
+cp manifest.yaml ${OFFLINE_DIR}
 
 cd ${OFFLINE_DIR}
 export KKZONE=cn
