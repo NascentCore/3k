dd7b71fab5f923355034e882076e1ef49bb58ab0
zhoujie <zhoujie16m@163.com>
2024-03-22 20:57:23 +0800

    fix: bugs (#690)

--- ui/src/pages/Grafana/index.tsx
+++ ui/src/pages/Grafana/index.tsx
@@ -2,7 +2,7 @@ import { PageContainer } from '@ant-design/pro-components';
 import React from 'react';
 
 const Admin: React.FC = () => {
-  const url = `${window.location.protocol}//${window.location.hostname}:300006`;
+  const url = `${window.location.protocol}//${window.location.hostname}:30006`;
   return (
     <PageContainer>
       <iframe src={url} style={{ width: '100%', height: 'calc(100vh - 170px)', border: 'none' }} />
