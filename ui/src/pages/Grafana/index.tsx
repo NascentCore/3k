09f4f367288c78bed6ef2213605c35e9035f744b
zhoujie <zhoujie16m@163.com>
2024-05-23 09:00:53 +0800

    Feature webui dev (#888)

--- ui/src/pages/Grafana/index.tsx
+++ ui/src/pages/Grafana/index.tsx
@@ -1,12 +1,15 @@
 import { PageContainer } from '@ant-design/pro-components';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const Admin: React.FC = () => {
-  const url = `${window.location.protocol}//${window.location.hostname}:30006`;
+  useEffect(() => {
+    (document as any).querySelector('main.ant-layout-content').style.padding = 0;
+  }, []);
+  const url = `${window.location.protocol}//${window.location.hostname}:30006/d/Oxed_c6Wz/nvidia-dcgm-exporter-dashboard?orgId=1`;
   return (
-    <PageContainer>
-      <iframe src={url} style={{ width: '100%', height: 'calc(100vh - 170px)', border: 'none' }} />
-    </PageContainer>
+    <div>
+      <iframe src={url} style={{ width: '100%', height: 'calc(100vh - 60px)', border: 'none' }} />
+    </div>
   );
 };
 
