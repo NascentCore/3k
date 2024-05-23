974f07156b66a68bc609a7636493ead7c558989a
zhoujie <zhoujie16m@163.com>
2024-05-23 15:09:00 +0800

    feat: 优化webui

--- ui/src/pages/UserJob/DetailModel/index.tsx
+++ ui/src/pages/UserJob/DetailModel/index.tsx
@@ -2,14 +2,33 @@ import React, { useState } from 'react';
 import { Button, Modal, Image } from 'antd';
 import { useIntl } from '@umijs/max';
 import demoImage from './assets/GPUpic.png';
+import moment from 'moment';
 
 const App: React.FC = ({ record }: any) => {
   const intl = useIntl();
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const webUrl = `http://grafana.llm.sxwl.ai:30003/d/a85faaa0-8ff6-4f11-ac27-24cbb0fa4ee9/job-detail?orgId=1&var-ns=${record.userId}&var-pod=${record.jobName}`;
   const showModal = () => {
     setIsModalOpen(true);
+  };
+
+  const openWeb = () => {
+    // 辅助函数：根据record计算from和to参数
+    const calculateParams = () => {
+      const from = moment(record.createTime).valueOf(); // 创建时间的时间戳
+      let to;
+      if (record.workStatus === 2) {
+        to = moment(record.updateTime).valueOf(); // 状态为2时，使用更新时间的时间戳
+      } else {
+        to = 'now'; // 否则，使用当前时间的时间戳
+      }
+      return { from, to };
+    };
+    // 构建webUrl
+    const webUrl = `http://grafana.llm.sxwl.ai:30003/d/a85faaa0-8ff6-4f11-ac27-24cbb0fa4ee9/job-detail?orgId=1&var-ns=${
+      record.userId
+    }&var-pod=${record.jobName}&from=${calculateParams().from}&to=${calculateParams().to}`;
+    window.open(webUrl);
   };
 
   const handleCancel = () => {
@@ -18,7 +37,7 @@ const App: React.FC = ({ record }: any) => {
 
   return (
     <>
-      <Button type="link" onClick={showModal}>
+      <Button type="link" onClick={openWeb}>
         {intl.formatMessage({
           id: 'pages.userJob.table.column.action.detail',
           // defaultMessage: '详情',
