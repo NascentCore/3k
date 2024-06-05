3aeb56c455b98849848a717cf4cac7392cbec085
guochenshu <cs@sxwl.ai>
2024-06-05 15:57:43 +0800

    uploader

---
+++ cmd/uploader/cmd/root.go
@@ -0,0 +1,30 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "uploader",
+	Short: "uploader for resource uploading in sxwl",
+	Long:  `uploader for resource uploading in sxwl`,
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+}
