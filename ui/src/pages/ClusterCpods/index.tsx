7623f8ca69e79d86a05ad4b198dfae0fceecb75e
zhoujie <zhoujie16m@163.com>
2024-06-14 10:12:31 +0800

    feat: webui 修改

--- ui/src/pages/ClusterCpods/index.tsx
+++ ui/src/pages/ClusterCpods/index.tsx
@@ -28,33 +28,23 @@ const Index: React.FC = () => {
           >
             <Table
               columns={[
+                // {
+                //   title: intl.formatMessage({
+                //     id: 'pages.ClusterCpods.table.column.cpod_id',
+                //     defaultMessage: 'CPod ID',
+                //   }),
+                //   dataIndex: 'cpod_id',
+                //   key: 'cpod_id',
+                //   align: 'center',
+                //   width: 150,
+                // },
                 {
                   title: intl.formatMessage({
-                    id: 'pages.ClusterCpods.table.column.cpod_id',
-                    defaultMessage: 'CPod ID',
+                    id: 'pages.ClusterCpods.table.column.gpu_prod',
+                    defaultMessage: 'GPU型号',
                   }),
-                  dataIndex: 'cpod_id',
-                  key: 'cpod_id',
-                  align: 'center',
-                  width: 150,
-                },
-                {
-                  title: intl.formatMessage({
-                    id: 'pages.ClusterCpods.table.column.cpod_version',
-                    defaultMessage: 'CPod版本',
-                  }),
-                  dataIndex: 'cpod_version',
-                  key: 'cpod_version',
-                  align: 'center',
-                  width: 150,
-                },
-                {
-                  title: intl.formatMessage({
-                    id: 'pages.ClusterCpods.table.column.gpu_allocatable',
-                    defaultMessage: '可分配GPU',
-                  }),
-                  dataIndex: 'gpu_allocatable',
-                  key: 'gpu_allocatable',
+                  dataIndex: 'gpu_prod',
+                  key: 'gpu_prod',
                   align: 'center',
                   width: 150,
                 },
@@ -70,16 +60,6 @@ const Index: React.FC = () => {
                 },
                 {
                   title: intl.formatMessage({
-                    id: 'pages.ClusterCpods.table.column.gpu_prod',
-                    defaultMessage: 'GPU型号',
-                  }),
-                  dataIndex: 'gpu_prod',
-                  key: 'gpu_prod',
-                  align: 'center',
-                  width: 150,
-                },
-                {
-                  title: intl.formatMessage({
                     id: 'pages.ClusterCpods.table.column.gpu_total',
                     defaultMessage: 'GPU总数',
                   }),
@@ -90,34 +70,55 @@ const Index: React.FC = () => {
                 },
                 {
                   title: intl.formatMessage({
-                    id: 'pages.ClusterCpods.table.column.gpu_vendor',
-                    defaultMessage: 'GPU厂商',
+                    id: 'pages.ClusterCpods.table.column.gpu_allocatable',
+                    defaultMessage: '可分配GPU',
                   }),
-                  dataIndex: 'gpu_vendor',
-                  key: 'gpu_vendor',
+                  dataIndex: 'gpu_allocatable',
+                  key: 'gpu_allocatable',
                   align: 'center',
                   width: 150,
                 },
-                {
-                  title: intl.formatMessage({
-                    id: 'pages.ClusterCpods.table.column.create_time',
-                    defaultMessage: '创建时间',
-                  }),
-                  dataIndex: 'create_time',
-                  key: 'create_time',
-                  align: 'center',
-                  width: 150,
-                },
-                {
-                  title: intl.formatMessage({
-                    id: 'pages.ClusterCpods.table.column.update_time',
-                    defaultMessage: '更新时间',
-                  }),
-                  dataIndex: 'update_time',
-                  key: 'update_time',
-                  align: 'center',
-                  width: 150,
-                },
+                // {
+                //   title: intl.formatMessage({
+                //     id: 'pages.ClusterCpods.table.column.cpod_version',
+                //     defaultMessage: 'CPod版本',
+                //   }),
+                //   dataIndex: 'cpod_version',
+                //   key: 'cpod_version',
+                //   align: 'center',
+                //   width: 150,
+                // },
+
+                // {
+                //   title: intl.formatMessage({
+                //     id: 'pages.ClusterCpods.table.column.gpu_vendor',
+                //     defaultMessage: 'GPU厂商',
+                //   }),
+                //   dataIndex: 'gpu_vendor',
+                //   key: 'gpu_vendor',
+                //   align: 'center',
+                //   width: 150,
+                // },
+                // {
+                //   title: intl.formatMessage({
+                //     id: 'pages.ClusterCpods.table.column.create_time',
+                //     defaultMessage: '创建时间',
+                //   }),
+                //   dataIndex: 'create_time',
+                //   key: 'create_time',
+                //   align: 'center',
+                //   width: 150,
+                // },
+                // {
+                //   title: intl.formatMessage({
+                //     id: 'pages.ClusterCpods.table.column.update_time',
+                //     defaultMessage: '更新时间',
+                //   }),
+                //   dataIndex: 'update_time',
+                //   key: 'update_time',
+                //   align: 'center',
+                //   width: 150,
+                // },
               ]}
               dataSource={data[title] || []}
               loading={isLoading}
