58b3964e3af640bec296daa24bfd4aa322d33abd
zhoujie <zhoujie16m@163.com>
2024-08-08 11:06:51 +0800

    feat: WebUI 修改模型仓库，数据集，适配器页面 (#1043)
    <img width="800" alt="image"
    src="https://github.com/user-attachments/assets/0a890d0c-91e2-499a-9152-78acefccf747">
    <img width="800" alt="image"
    src="https://github.com/user-attachments/assets/b06b8168-f2f4-489c-b3b5-5cc3ade06499">
    <img width="800" alt="image"
    src="https://github.com/user-attachments/assets/b1db27b8-c98d-4800-9859-acc445c9baac">

--- ui/src/locales/en-US/global.ts
+++ ui/src/locales/en-US/global.ts
@@ -2,6 +2,7 @@
  * 通用部分 国际化
  */
 export default {
+  'pages.global.header.accountDetail': 'Account Details',
   'pages.global.header.logout': 'Log Out',
   'pages.global.confirm.title': 'Prompt',
   'pages.global.confirm.okText': 'Yes',
@@ -14,4 +15,5 @@ export default {
   'pages.global.form.select.placeholder': 'Please select',
   'pages.golbal.table.column.action': 'Action',
   'pages.golbal.gpu.select.option.disabled.placeholder': 'Recharge over 10000 can be selected',
+  'pages.global.button.upload': 'Upload',
 };
