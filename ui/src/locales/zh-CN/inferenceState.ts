d760674e2004df5667bd8d2e59c111f54e705e9f
Yaxiong Zhao <142474494+yzhao-2023@users.noreply.github.com>
2024-09-01 20:01:16 -0700

    Update inferenceState.ts

--- ui/src/locales/zh-CN/inferenceState.ts
+++ ui/src/locales/zh-CN/inferenceState.ts
@@ -2,9 +2,9 @@
  * 推理服务状态 国际化
  */
 export default {
-  'pages.inferenceState.table.column.service_name': '推理服务名称',
+  'pages.inferenceState.table.column.service_name': '推理实例',
   'pages.inferenceState.table.column.model_id': '模型名称',
-  'pages.inferenceState.table.column.status': '推理服务状态',
+  'pages.inferenceState.table.column.status': '推理实例状态',
   'pages.inferenceState.table.column.url': 'url',
   'pages.inferenceState.table.column.start_time': '启动时间',
   'pages.inferenceState.table.column.end_time': '终止时间',
