bafcd6301a0f511dd94b657ed0c6bc58599b663d
cairong <146914987+cairong-ai@users.noreply.github.com>
2024-10-14 09:00:52 +0800

    Update applicationMenu.ts

--- ui/src/locales/zh-CN/applicationMenu.ts
+++ ui/src/locales/zh-CN/applicationMenu.ts
@@ -4,7 +4,7 @@
 export default {
   'pages.applicationMenu.darwer.title.deployApp': '部署应用',
   'pages.applicationMenu.appCard.status': '状态',
-  'pages.applicationMenu.appCard.access': '访问知识库库',
+  'pages.applicationMenu.appCard.access': '访问知识库',
   'pages.applicationMenu.appCard.deployment': '部署',
   'pages.applicationMenu.appCard.onDeployment': '部署中',
   'pages.applicationMenu.appAddForm.form.app_name': '应用名称',
