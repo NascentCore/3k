0ce99adb5607a5ce244fc656b182136fbeda9654
cairong <146914987+cairong-ai@users.noreply.github.com>
2024-10-16 13:19:38 +0800

    add llm integrated for rag app (#1097)
    1. add llm integrated for rag app
    2. add copy api endpoint for inference instance
    3. add locales config

--- ui/src/locales/en-US/inferenceState.ts
+++ ui/src/locales/en-US/inferenceState.ts
@@ -11,7 +11,10 @@ export default {
   'pages.inferenceState.table.column.end_time': 'End Time',
   'pages.inferenceState.table.column.action': 'Action',
   'pages.inferenceState.table.column.action.startChat': 'Start Chat',
+  'pages.inferenceState.table.column.action.copyApiEndPoint': 'API EndPoint',
   'pages.inferenceState.table.column.action.stop': 'Stop',
   'pages.inferenceState.table.column.action.stop.confirm': 'Confirm to stop?',
-  'pages.inferenceState.table.column.action.stop.success': 'Operation successful',
+  'pages.inferenceState.table.column.action.stop.success': 'Operation Success',
+  'pages.inferenceState.table.column.action.copy': 'Copy',
+  'pages.inferenceState.table.column.action.copy.success': 'Copy Success',
 };
--- ui/src/locales/zh-CN/applicationMenu.ts
+++ ui/src/locales/zh-CN/applicationMenu.ts
@@ -8,4 +8,6 @@ export default {
   'pages.applicationMenu.appCard.deployment': '部署',
   'pages.applicationMenu.appCard.onDeployment': '部署中',
   'pages.applicationMenu.appAddForm.form.app_name': '应用名称',
+  'pages.applicationMenu.appAddForm.form.inference_select': '推理实例',
+  'pages.applicationMenu.appAddForm.form.inference_select_placeholder': '请选择推理服务实例',
 };
